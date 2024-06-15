5ccd2413beadf8bddb0e0f56f1c071357198489d
asndev <asndev99@gmail.com>
2024-06-15 23:16:58 +0500

    user module completed

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Param,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from 'src/schemas/UserSchema';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +19,28 @@ export class UsersController {
   @Post('create')
   async createUser(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.userService.createUser(createUserDto);
+  }
+
+  @Get()
+  async getUsers(): Promise<User[] | []> {
+    return this.userService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<User | null> {
+    return this.userService.getUserById(id);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto,
+  ): Promise<User | null> {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { HttpException, Injectable, Param } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { User } from 'src/schemas/UserSchema';
 import { CreateUserDto } from './dto/create-user.dto';
 
@@ -12,6 +13,60 @@ export class UsersService {
     try {
       const newUser = await this.userModel.create(createUserDto);
       return newUser.save();
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  async getUsers() {
+    try {
+      return this.userModel.find();
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  async getUserById(id: string) {
+    try {
+      const isValid = mongoose.Types.ObjectId.isValid(id);
+      if (!isValid) {
+        throw new HttpException('User not found', 404);
+      }
+      const user = await this.userModel.findById(id);
+      if (!user) {
+        throw new HttpException('User not found', 404);
+      }
+      return user;
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  async updateUser(id: string, updateUserDto: UpdateUserDto) {
+    try {
+      const isValid = mongoose.Types.ObjectId.isValid(id);
+      if (!isValid) {
+        throw new HttpException('User not found', 404);
+      }
+      return await this.userModel.findByIdAndUpdate(id, updateUserDto, {
+        new: true,
+      });
+    } catch (error) {
+      console.log(error);
+      throw new Error(error);
+    }
+  }
+
+  async deleteUser(id: string) {
+    try {
+      const isValid = mongoose.Types.ObjectId.isValid(id);
+      if (!isValid) {
+        throw new HttpException('Invalid id', 404);
+      }
+      return await this.userModel.findByIdAndDelete(id);
     } catch (error) {
       console.log(error);
       throw new Error(error);
